ecdacbb5a77955d8baf370b78d7be768bdfb7a45
Asif294 <asifurrahman677294@gmail.com>
2025-09-07 11:05:49 +0600

    lamda

---
+++ buildin_moduls.py
@@ -0,0 +1,11 @@
+from math import *
+from random import *
+from time import sleep
+result =ceil(4.021) #upper value
+print (result)
+result =floor(4.251)
+print(result)
+sleep(4)
+print(choice(['asif','nijam','moinul','noman','arafat']))
+
+
---
+++ lamda.py
@@ -0,0 +1,16 @@
+# def double(num):
+#     return num*2
+
+
+
+double=lambda  num:num*2
+square =lambda num:num*num
+s_result=square(8)
+result=double(33)
+print(s_result)
+print(result)
+
+# more paramiter lamda function
+add =lambda x,y : x+y
+sum=add(7400,5)
+print(sum)
